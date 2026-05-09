85246c3bbffb4cc98405031a5a4dd0cc6925571e
Sweta Dahal <dahalsweta5@gmail.com>
2026-05-09 11:08:53 +0545

    Database creation

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -5,15 +5,15 @@ import { User } from "../entities/User";
 dotenv.config();
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: process.env.DB_HOST || "localhost",
-    port: parseInt(process.env.DB_PORT || "5432"),
-    username: process.env.DB_USERNAME || "postgres",
-    password: process.env.DB_PASSWORD || "password",
-    database: process.env.DB_DATABASE || "sweta",
-    synchronize: true, // Auto create db schema based on entities
-    logging: false,
-    entities: [User],
-    subscribers: [],
-    migrations: [],
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  synchronize: true,
+  logging: true,
+  entities: [User],
+  subscribers: [],
+  migrations: [],
 });
--- backend/src/entities/User.ts
+++ backend/src/entities/User.ts
@@ -2,7 +2,7 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn("int")
+    @PrimaryGeneratedColumn("increment")
     id!: number
 
     @Column("varchar")
